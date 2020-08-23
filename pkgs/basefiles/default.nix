f61030392bd8e169b70881ccf172cd033506a241
Christine Dodrill <me@christine.website>
2020-08-23 10:57:11 -0400

    basefiles from pkg.musl.cc

--- pkgs/basefiles/default.nix
+++ pkgs/basefiles/default.nix
@@ -1,6 +1,7 @@
 { stdenv, sinit, startup }:
 
-stdenv.mkDerivation {
+let sources = import ../../nix/sources.nix;
+in stdenv.mkDerivation {
   pname = "basefiles";
   version = "mara";
   src = ./src;
@@ -17,6 +18,12 @@ stdenv.mkDerivation {
     # packages
     cp ${sinit}/bin/sinit $out/bin/sinit
     cp ${startup}/rc/startup $out/rc/startup
+
+    # basefiles from musl.cc
+    cp ${sources.mount} $out/bin/mount
+    chmod +x $out/bin/mount
+    cp ${sources.ip} $out/bin/ip
+    chmod +x $out/bin/ip
 
     # passwd
     cp $src/etc/passwd $out/etc/passwd
